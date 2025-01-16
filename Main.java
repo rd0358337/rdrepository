7cd83dae9c5188783f64c6719736b3b8ac9c0b58
rd0358337 <rd0358337@otc.edu>
2025-01-16 12:27:26 -0600

    commit

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Player constructor
+        //Player object
         Player player = new Player("RD", 100);
 
         //Player details output
